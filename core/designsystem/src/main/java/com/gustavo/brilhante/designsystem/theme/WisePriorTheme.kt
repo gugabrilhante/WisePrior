2460426a83e33d9d05197ce99c3292b24f8213cf
gustavobrilhante <gugabrilhante@gmail.com>
2026-05-13 11:33:53 -0300

    fix play store warning

--- core/designsystem/src/main/java/com/gustavo/brilhante/designsystem/theme/WisePriorTheme.kt
+++ core/designsystem/src/main/java/com/gustavo/brilhante/designsystem/theme/WisePriorTheme.kt
@@ -1,5 +1,6 @@
 package com.gustavo.brilhante.designsystem.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,8 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.colorResource
+import androidx.core.view.WindowCompat
 import com.gustavo.brilhante.designsystem.R
 
 @Composable
@@ -60,6 +65,15 @@ fun WisePriorTheme(
         }
         darkTheme -> darkColorScheme
         else -> lightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
+        }
     }
 
     MaterialTheme(
